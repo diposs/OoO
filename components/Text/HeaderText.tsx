17caf76b0d06c2d321298d21d7b8f6385a3857f3
diposs <onakunle.dipo@gmail.com>
2023-10-11 17:16:31 +0100

    Update HeaderText.tsx

--- components/Text/HeaderText.tsx
+++ components/Text/HeaderText.tsx
@@ -1,9 +1,11 @@
-import { Title } from '@mantine/core';
+import { Title, useMantineColorScheme } from '@mantine/core';
 
 export const Texto = () => {
+  const { colorScheme } = useMantineColorScheme();
   return (
     <Title
       sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+      color = {colorScheme === 'dark' ? 'yellow.5' : 'grape.8'}
     >
       &#9854;
     </Title>
