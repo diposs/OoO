79dd4573996292b35144eaaa495fd08327947414
diposs <onakunle.dipo@gmail.com>
2023-10-11 17:36:41 +0100

    Update GSButton.tsx

--- components/buttons/GSButton.tsx
+++ components/buttons/GSButton.tsx
@@ -1,12 +1,19 @@
-import { Button, useMantineColorScheme  } from "@mantine/core";
+import { Button, useMantineColorScheme, Group  } from "@mantine/core";
+import {ColorSchemeToggle } from "../ColorSchemeToggle"
 export const GsButton = ({ ...rest }) =>  {
     const { colorScheme } = useMantineColorScheme();
     return (
-        <Button radius="xl" h={40} color={colorScheme === 'dark' ? 'teal.4' : 'blue.6' } { ...rest }>GetStarted</Button>
+        <Group spacing="lg">
+            <ColorSchemeToggle />
+            <Button radius="xl" h={40} color={colorScheme === 'dark' ? 'teal.4' : 'blue.6' } { ...rest }>GetStarted</Button>
+        </Group>
     );
 }
 export const GsLogoutButton = ({ ...rest }) =>  {
     return (
-        <Button radius="md" h={40}  { ...rest }>LogOut</Button>
+        <Group spacing="lg">
+            <ColorSchemeToggle />
+            <Button radius="md" h={40}  { ...rest }>LogOut</Button>
+        </Group>
     );
 }
