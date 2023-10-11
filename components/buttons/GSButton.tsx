290892a7fb9e966282a33afa6d229cf0c17d7c22
diposs <onakunle.dipo@gmail.com>
2023-10-11 20:50:22 +0100

    Update GSButton.tsx

--- components/buttons/GSButton.tsx
+++ components/buttons/GSButton.tsx
@@ -1,15 +1,15 @@
-import { Button, useMantineColorScheme, Group, packSx, DefaultProps  } from "@mantine/core";
+import { Button, useMantineColorScheme, Group, Sx } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import {ColorSchemeToggle } from "../ColorSchemeToggle";
 
-interface GsButtonProps extends DefaultProps {}
-interface GsLogoutButtonProps extends DefaultProps {}
+interface GsButtonProps {sx?:Sx}
+interface GsLogoutButtonProps {sx?:Sx}
 
 export const GsButton = ({ sx, ...rest }:GsButtonProps) =>  {
     const { colorScheme } = useMantineColorScheme();
     const matches = useMediaQuery('(min-width: 48em)');
     return (
-        <Group spacing="lg" sx={[...packSx(sx)]}>
+        <Group spacing="lg" sx={[sx]}>
             <ColorSchemeToggle sx={{display: matches ? 'flex' : 'none'}} />
             <Button radius="xl" h={40} color={colorScheme === 'dark' ? 'teal.4' : 'blue.6' } { ...rest }>GetStarted</Button>
         </Group>
@@ -19,7 +19,7 @@ export const GsLogoutButton = ({ sx, ...rest }:GsLogoutButtonProps) =>  {
     const { colorScheme } = useMantineColorScheme();
     const matches = useMediaQuery('(min-width: 48em)');
     return (
-        <Group spacing="lg" sx={[...packSx(sx)]}>
+        <Group spacing="lg" sx={[sx]}>
             <ColorSchemeToggle sx={{display: matches ? 'flex' : 'none'}} />
             <Button radius="md" h={40} color={colorScheme === 'dark' ? 'teal.4' : 'blue.6' }  { ...rest }>LogOut</Button>
         </Group>
