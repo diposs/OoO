4beb45a4dddcf4004943a3c5adbf9caefa125033
diposs <onakunle.dipo@gmail.com>
2023-10-11 20:04:25 +0100

    Update GSButton.tsx

--- components/buttons/GSButton.tsx
+++ components/buttons/GSButton.tsx
@@ -1,19 +1,23 @@
 import { Button, useMantineColorScheme, Group  } from "@mantine/core";
-import {ColorSchemeToggle } from "../ColorSchemeToggle"
+import { useMediaQuery } from "@mantine/hooks";
+import {ColorSchemeToggle } from "../ColorSchemeToggle";
 export const GsButton = ({ ...rest }) =>  {
     const { colorScheme } = useMantineColorScheme();
+    const matches = useMediaQuery('(min-width: 48em)');
     return (
         <Group spacing="lg">
-            <ColorSchemeToggle { ...rest } />
+            <ColorSchemeToggle sx={(theme) => ({display: matches ? 'flex' : 'none',})} />
             <Button radius="xl" h={40} color={colorScheme === 'dark' ? 'teal.4' : 'blue.6' } { ...rest }>GetStarted</Button>
         </Group>
     );
 }
 export const GsLogoutButton = ({ ...rest }) =>  {
+    const { colorScheme } = useMantineColorScheme();
+    const matches = useMediaQuery('(min-width: 48em)');
     return (
         <Group spacing="lg">
-            <ColorSchemeToggle { ...rest } />
-            <Button radius="md" h={40}  { ...rest }>LogOut</Button>
+            <ColorSchemeToggle sx={(theme) => ({display: matches ? 'flex' : 'none',})} />
+            <Button radius="md" h={40} color={colorScheme === 'dark' ? 'teal.4' : 'blue.6' }  { ...rest }>LogOut</Button>
         </Group>
     );
 }
