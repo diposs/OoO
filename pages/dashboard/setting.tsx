94f471cf4ef02f3a45fe283b959a31cf79e5c288
diposs <onakunle.dipo@gmail.com>
2023-10-12 11:40:27 +0100

    Create setting.tsx]

---
+++ pages/dashboard/setting.tsx
@@ -0,0 +1,11 @@
+import {FirstHeader} from '../components/header/header1';
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle';
+export default function Home() {
+  return (
+    <>
+      <FirstHeader/>
+      <ColorSchemeToggle />
+      </>
+   
+  );
+}
