f973005be3e5bbbb053800622a0164c1c57fae71
diposs <onakunle.dipo@gmail.com>
2023-10-12 10:06:52 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
     setColorScheme(nextColorScheme);
     setCookie('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
   };
-  setColorScheme(getCookie('mantine-color-scheme') || 'light');
+  setColorScheme(getCookie('mantine-color-scheme') as ColorScheme || 'light' as ColorScheme);
  const { pvKey } = useBoundStore3();
   useEffect(() => {
     polybase.signer(async (data) => {
