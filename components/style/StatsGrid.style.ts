3048261080d9682cd6de2836c247b4b0b4f91701
diposs <onakunle.dipo@gmail.com>
2023-10-16 11:34:58 +0100

    Create StatsGrid.style.ts

---
+++ components/style/StatsGrid.style.ts
@@ -0,0 +1,30 @@
+import { createStyles } from '@mantine/core';
+
+
+export default createStyles ((theme) => ({
+  root: {
+  padding: theme.spacing.xl * 1.5,
+},
+
+value: {
+  fontSize: rem(24px),
+  fontWeight: 700,
+  lineHeight: 1,
+},
+
+diff: {
+  lineHeight: 1,
+  display: flex,
+  alignItems: center,
+},
+
+icon: {
+  color: theme.colorScheme === 'dark' ? theme.colors.gray[3] : theme.colors.gray[4],
+},
+
+title: {
+  fontWeight: 700,
+  textTransform: uppercase,
+},
+})
+)
