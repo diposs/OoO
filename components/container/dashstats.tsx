c42686eace731fabde0f012fa3a2fa68083e16aa
diposs <onakunle.dipo@gmail.com>
2023-10-16 20:17:14 +0100

    Update dashstats.tsx

--- components/container/dashstats.tsx
+++ components/container/dashstats.tsx
@@ -60,7 +60,7 @@ export function StatsGrid() {
         <Button
         variant="subtle"
         component="a"
-        href="/settings"
+        href="../dashboard/settings"
         leftIcon={<IconSettings size={rem(18)} />}
       >
         <Text size="xs"  className={classes.title}>Settings</Text>
