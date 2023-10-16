33e0c324aa6923b9778ac14a232a89354759a373
diposs <onakunle.dipo@gmail.com>
2023-10-16 13:09:18 +0100

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,15 @@
 import {FirstHeader} from '../components/header/header1';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle';
 import { StatsGrid } from '../components/container/dashstats';
+import { AccDrop } from '../components/container/acc-drop';
+
 export default function Home() {
   return (
     <>
       <FirstHeader/>
       <ColorSchemeToggle />
       <StatsGrid/>
+      <AccDrop/>
     </>
    
   );
