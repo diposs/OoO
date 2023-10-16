e1e8513f02dcb96801f4ad84e5eda899b08b416b
diposs <onakunle.dipo@gmail.com>
2023-10-16 12:51:18 +0100

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,13 @@
 import {FirstHeader} from '../components/header/header1';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle';
+import { StatsGrid } from '../components/container/dashstats';
 export default function Home() {
   return (
     <>
       <FirstHeader/>
       <ColorSchemeToggle />
-      </>
+      <StatsGrid/>
+    </>
    
   );
 }
