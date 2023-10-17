cc8cf8927c3d6d8fb6f06d6c8c1bfb4f4ec3ae19
diposs <onakunle.dipo@gmail.com>
2023-10-17 11:24:46 +0100

    Update menucontent.ts

--- components/menu/menucontent.ts
+++ components/menu/menucontent.ts
@@ -2,13 +2,13 @@ export const items:{link:string;label:string;links?:{link:string;label:string;}[
 [
 {"link":"../","label":"Home"},
 {"link":"../marketplace","label":"MarketPlace"},
-{"link":"/waterfall","label":"WaterFall"},
-{"link":"/pricing","label":"Flow"},
+{"link":"../waterfall","label":"WaterFall"},
+{"link":"../flow","label":"Flow"},
 {"link":"#2","label":"Support",
 "links":[
-{"link":"/faq","label":"FAQ"},
-{"link":"/support-talks","label":"Chat with Support"},
-{"link":"/OoO","label":"OoO"}
+{"link":"../faq","label":"FAQ"},
+{"link":"../support-talks","label":"Chat with Support"},
+{"link":"../OoO","label":"OoO"}
 ]
 }
 ];
